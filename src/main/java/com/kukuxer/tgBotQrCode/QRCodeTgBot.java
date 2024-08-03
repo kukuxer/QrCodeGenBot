eefd2448c5fc3474cc131ae28ccdfa3be0d707eb
Hlib Chibichian <chibgleb99@gmail.com>
2024-08-03 21:30:18 +0100

    added some logic

--- src/main/java/com/kukuxer/tgBotQrCode/QRCodeTgBot.java
+++ src/main/java/com/kukuxer/tgBotQrCode/QRCodeTgBot.java
@@ -6,19 +6,34 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 
 import java.awt.*;
+import java.util.List;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class QRCodeTgBot extends TelegramLongPollingBot {
     private final TelegramBotConfig telegramBotConfig;
     private final QRCodeGenerator qrCodeGenerator;
     private final UserService userService;
+
+    public QRCodeTgBot(
+            TelegramBotConfig telegramBotConfig,
+            QRCodeGenerator qrCodeGenerator,
+            UserService userService
+            ){
+        this.telegramBotConfig = telegramBotConfig;
+        this.qrCodeGenerator = qrCodeGenerator;
+        this.userService = userService;
+        generateMenuButtons();
+    }
 
     @Override
     public String getBotUsername() {
@@ -40,22 +55,20 @@ public class QRCodeTgBot extends TelegramLongPollingBot {
             processCommands(user, text);
         }
 
-
-        InputFile qrFile = qrCodeGenerator.getQRCodeImageFile(messageText, Color.RED, Color.BLACK);
-        SendPhoto sendPhoto = new SendPhoto();
-        sendPhoto.setChatId(chatId.toString());
-        sendPhoto.setPhoto(qrFile);
-        execute(sendPhoto);
-
+        if() {
+            InputFile qrFile = qrCodeGenerator.getQRCodeImageFile(text, Color.RED, Color.BLACK);
+            SendPhoto sendPhoto = new SendPhoto();
+            sendPhoto.setChatId(chatId.toString());
+            sendPhoto.setPhoto(qrFile);
+            execute(sendPhoto);
+        }
     }
 
 
     private boolean isCommand(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String text = update.getMessage().getText();
-            if (text.startsWith("/")) {
-                return true;
-            }
+            return text.startsWith("/");
         }
         return false;
     }
@@ -66,8 +79,12 @@ public class QRCodeTgBot extends TelegramLongPollingBot {
                     user,
                     "Hello, welcome to the bot, that will help you to create your qr codes!"
             );
-
+            case "/generateqrcode" -> processQRCodeGenerating(user, text);
         }
+    }
+
+    private void processQRCodeGenerating(TgUser user, String text) {
+        sendMessageToUser(user,"Choose QR code type: ");
 
     }
 
@@ -77,6 +94,24 @@ public class QRCodeTgBot extends TelegramLongPollingBot {
                 .chatId(user.getChatId().toString())
                 .text(text)
                 .build()
+        );
+    }
+
+    @SneakyThrows
+    public void generateMenuButtons(){
+        List<BotCommand> listOfCommands = List.of(
+                new BotCommand("/start", "start to work with bot."),
+                new BotCommand("/profile", "open your profile"),
+                new BotCommand("/menu", "open bot menu"),
+                new BotCommand("/help", "bot usage guide"),
+                new BotCommand("/generateqrcode", "create your QR code")
+        );
+        this.execute(
+          new SetMyCommands(
+                  listOfCommands,
+                  new BotCommandScopeDefault(),
+                  null
+          )
         );
     }
 

f045c13d3c7472a0dc885a6294c829d5f3ded2e6
Yevhenii <999.haha.990@gmail.com>
2024-08-05 15:34:10 +0100

    expiration date checking + ip from russia

--- src/main/java/com/kukuxer/tgBotQrCode/qrcode/QrCodeController.java
+++ src/main/java/com/kukuxer/tgBotQrCode/qrcode/QrCodeController.java
@@ -7,26 +7,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.UUID;
+
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/redirect")
 public class QrCodeController {
     private final QrCodeRepository qrCodeRepository;
     private final QrCodeVisitorService qrCodeVisitorService;
 
-    @GetMapping("/redirect/{qrCodeId}")
-    public ResponseEntity<?> redirect(@PathVariable("qrCodeId") String qrCodeIdString, HttpServletRequest request) {
+    @GetMapping("/{qrCodeId}")
+    public String redirect(@PathVariable("qrCodeId") String qrCodeIdString, HttpServletRequest request, Model model) {
         try {
             UUID qrCodeId = UUID.fromString(qrCodeIdString);
 
             QrCode qrCode = qrCodeRepository.findById(qrCodeId)
                     .orElseThrow(() -> new RuntimeException("QR code not found"));
+
+            if (qrCode.getExpirationTime().isBefore(LocalDateTime.now())) {
+                model.addAttribute("qrCode", qrCode.getQrCodeScanCount());
+                model.addAttribute("expirationDate", qrCode.getExpirationTime());
+                model.addAttribute("creationDate", qrCode.getCreationDate());
+                model.addAttribute("creatorNickname", qrCode.getCreator().getTgUsername());
+                return "expired";
+            }
 
             String ipAddress = request.getRemoteAddr();
             QrCodeVisitor qrCodeVisitor = qrCodeVisitorService.createQrCodeVisitorByIpOrElseCreateNew(ipAddress, qrCode);
@@ -35,31 +50,29 @@ public class QrCodeController {
             qrCode.setQrCodeScanCount(qrCode.getQrCodeScanCount() + 1);
             qrCodeRepository.save(qrCode);
 
-            String url = qrCode.getText();
-            URI redirectUri = new URI(url);
-
-            if (isValidUrl(url)) {
-                return ResponseEntity.status(HttpStatus.FOUND)
-                        .header("Location", redirectUri.toString())
-                        .build();
-            } else {
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body("QR Code Text: " + qrCode.getText());
+            if ("Russia".equalsIgnoreCase(qrCodeVisitor.getCountry())) {
+                model.addAttribute("ipAddress", qrCodeVisitor.getIp());
+                model.addAttribute("city", qrCodeVisitor.getCity());
+                return "russkiy_idi_nahui";
             }
-        } catch (URISyntaxException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Invalid URL: " + qrCodeIdString);
+
+            String url = qrCode.getText();
+            URI redirectUri;
+            try {
+                redirectUri = new URI(url);
+                if (!redirectUri.isAbsolute() || redirectUri.getScheme() == null) {
+                    throw new URISyntaxException(url, "Invalid URL scheme");
+                }
+            } catch (URISyntaxException e) {
+                model.addAttribute("message", "Invalid URL: " + url);
+                return "message";
+            }
+
+            return "redirect:" + url;
+
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body("QR Code Text: " + qrCodeIdString);
+            model.addAttribute("message", "Error processing QR Code: " + e.getMessage());
+            return "message";
         }
     }
-    private boolean isValidUrl(String url) {
-        try {
-            new URI(url).parseServerAuthority();
-            return true;
-        } catch (URISyntaxException e) {
-            return false;
-        }
-}
 }
